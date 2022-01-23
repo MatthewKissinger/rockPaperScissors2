5f59ad74998b72d810d4e09b617ca09cebecd018
Matt <matthew.t.kissinger@gmail.com>
2022-01-22 21:06:06 -0800

    added Round picking feature -mk

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// TODO List
+// 1 - add in start game and continue game features
 
+// Dom Cache
+let gameStart = document.getElementById('gameStart');
 
+// Event Listeners
+
+gameStart.addEventListener("click", function() {
+    roundTotal();
+} );
+
+// global variables
+let userScore = 0;
+let computerScore = 0;
+let numRounds;
 
 
 function userPlay() {
@@ -45,27 +59,81 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         result = `It's a draw`;
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        userScore++;
         result = `You win`;
     } else if (playerSelection == `rock`&& computerSelection == `paper`) {
+        computerScore++;
         result = `The computer wins`;
     } else if (playerSelection == `paper` && computerSelection == `rock`) {
+        userScore++;
         result = `You win`;
     } else if (playerSelection == `paper` && computerSelection == `scissors`) {
+        computerScore++;
         result = `The computer wins`;
     } else if (playerSelection == `scissors` && computerSelection == `paper`) {
+        userScore++;
         result = `You win`;
     } else {
+        computerScore++;
         result = `The computer wins`;
     }
-
+    console.log(result);
     return result;
 }
 
-// const playerSelection = userPlay();
-// const computerSelection = computerPlay();
+function roundTotal() {
+    numRounds = prompt(`Enter 1, 3, or 5 for the game length`);
 
-//console.log(playRound(playerSelection, computerSelection));
+    if (numRounds == null) {
+        alert(`Ending game start sequence`);
+        return;
+    }
+
+    numRounds = parseInt(numRounds);
+    
+    if (numRounds === 1 || numRounds === 3 || numRounds === 5) {
+        console.log(numRounds);
+        console.log(typeof(numRounds));
+        alert(`Start the Game!`);
+        return numRounds;
+
+    } else {
+        alert(`You have picked an invalid total`);
+        roundTotal();
+        return numRounds;
+    }
+}
 
 function game(numRounds) {
     
+    let round = 1;
+
+    let playerSelection;
+    let computerSelection;
+
+    for (let i = round; i < numRounds + 1; i++) {
+        console.log(`Round ${round}`);
+        console.log(`User Score is ${userScore}`);
+        console.log(`Computer Score is ${computerScore}`);
+
+        playerSelection = userPlay();
+        computerSelection = computerPlay();
+
+        playRound(playerSelection, computerSelection);
+        round++;
+    }
+
+    console.log(`End of Game`);
+    console.log(`User Score Total = ${userScore}`);
+    console.log(`Computer Score Total = ${computerScore}`);
+
+    if (userScore === computerScore) {
+        console.log(`It's a draw`);
+    } else if (userScore > computerScore) {
+        console.log(`You win!!`);
+    } else {
+        console.log(`The computer wins`);
+    }
 }
+
+// game(5);
